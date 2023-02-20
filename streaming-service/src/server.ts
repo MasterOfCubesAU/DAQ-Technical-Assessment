63f10b6610fa6023e338b0aa187f2b14a59b6eae
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-02-20 16:53:54 +1100

    Added file logging for unsafe battery events

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -1,10 +1,36 @@
 import net from 'net';
 import { WebSocket, WebSocketServer } from 'ws';
+import pino from "pino";
+import prettyPino from "pino-pretty";
+import SonicBoom from "sonic-boom";
 
 const TCP_PORT = parseInt(process.env.TCP_PORT || '12000', 10);
 
 const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
+
+const MIN_OPERATING_RANGE = 20;
+const MAX_OPERATING_RANGE = 80;
+
+const MAX_BATTERY_WARNINGS = 3;
+const BATTERY_WARNING_PERIOD = 5000;
+
+const batteryTemperatureWarnings: number[] = [];
+
+const LOGGER = pino(prettyPino({ destination: new SonicBoom({ dest: "./logs/incident.log", mkdir: true }), colorize: false }));
+
+// Incoming data interface
+interface VehicleData {
+    battery_temperature: number,
+    timestamp: number
+}
+
+function batteryExceedLimit(): boolean {
+    const currentTime = Date.now();
+    batteryTemperatureWarnings.filter(time => currentTime - time <= BATTERY_WARNING_PERIOD)
+    batteryTemperatureWarnings.push(currentTime)
+    return batteryTemperatureWarnings.length > MAX_BATTERY_WARNINGS
+}
 
 tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
@@ -12,11 +38,22 @@ tcpServer.on('connection', (socket) => {
     socket.on('data', (msg) => {
         console.log(msg.toString());
 
+        let jsonData: VehicleData | null = null;
+
         // Added try catch to see if the incoming str can be parsed to JSON. If not, we simply return
         try {
-            JSON.parse(msg.toString());
+            jsonData = JSON.parse(msg.toString());
         } catch (error) {
             return;
+        }
+
+        // Check if temps exceed safe operating range
+        if (jsonData && (jsonData.battery_temperature < MIN_OPERATING_RANGE || jsonData.battery_temperature > MAX_OPERATING_RANGE)) {
+            if (batteryExceedLimit()) {
+                console.log("log");
+
+                LOGGER.warn(`Battery exceeded safe operating temperature > ${MAX_BATTERY_WARNINGS} times within ${BATTERY_WARNING_PERIOD}ms`)
+            }
         }
 
         websocketServer.clients.forEach(function each(client) {
